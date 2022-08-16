2fc1956c01f9753533c32bd2b85e1a5e706fad24
hritk <ritikrj1@gmail.com>
2022-08-16 18:12:00 +0530

    m

--- server.js
+++ server.js
@@ -1,18 +1,40 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const morgan = require('morgan');
+const connectDB = require('./config/db'); 
+const colors = require('colors');
 
-const bootcamps = require('./router/bootcamps');
+
 
 dotenv.config({path: './config/config.env' });
 
+
+connectDB();
+
+
+const bootcamps = require('./router/bootcamps');
+
 const app = express();
 
+app.use(express.json());
+
+if(process.env.NODE_ENV === 'development'){
+    app.use(morgan('dev'));
+}
 
 app.use('/api/v1/bootcamps', bootcamps);
 
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, 
-    console.log(`server on ${PORT}`)
+const server = app.listen(PORT, 
+    console.log(`server in mode ${process.env.NODE_ENV} on ${PORT}`.yellow.bold)
     );
+
+    process.on('unhandledRejection', (err,promise) => {
+      
+        console.log(`Error : ${err.message}`.red); 
+
+        server.close( () => process.exit(1));
+
+    });
